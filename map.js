45ffd591bc141499d6cb9ef3c87e7447293011fe
Mohit verma <mk0641137@gmail.com>
2026-01-11 22:52:41 +0530

    Learn something new

---
+++ map.js
@@ -0,0 +1,25 @@
+let main = ["apple", "banana", "mango"];
+let Voice = [{name:"A", age:20}, {name:"B", age:25}]
+let arr = [1,2,3];
+// let value = arr.map( (arr) => arr * 2 
+// );
+// console.log(value);
+
+// ------------------------------------------k--------------------------------------
+
+// let mainFile = arr.map( (arr) => arr.toString() )
+// console.log(mainFile);
+
+// ----------------------------------------------k-------------------------------------
+
+
+// let sizeOwn = arr.map((arr) => arr + 10 )
+// console.log(sizeOwn);
+
+// ----------------------------------------k-------------------------------------------
+
+// let arrayName = main.map( (main) => main.toUpperCase() )
+// console.log(arrayName);
+
+// let userName = Voice.map( (Voice) => Voice.name  )
+// console.log(userName);
