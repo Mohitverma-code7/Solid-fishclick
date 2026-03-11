5ea6593e02c77c7edb8889f9510b337274863fb6
Mohit verma <mk0641137@gmail.com>
2026-03-12 01:06:11 +0530

    create some buttons

---
+++ Learning-fetch/fetch.js
@@ -0,0 +1,8 @@
+fetch("https://api.example.com/data")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+}).catch(error => {
+    console.log("Error:",error);
+    
+});
--- This/this.js
+++ This/this.js
@@ -1,66 +1,66 @@
-console.log(this);
+// console.log(this);
 
-function ranveerGalobalStage() {
-  return typeof this;
-}
-console.log(ranveerGalobalStage());
+// function ranveerGalobalStage() {
+//   return typeof this;
+// }
+// console.log(ranveerGalobalStage());
 
-function ranveerWithNoScript() {
-  "use strict";
-  return this;
-}
-console.log(ranveerWithNoScript());
+// function ranveerWithNoScript() {
+//   "use strict";
+//   return this;
+// }
+// console.log(ranveerWithNoScript());
 
-const bollywood = {
-  name: "Dahnu",
-  lead: "vijay",
+// const bollywood = {
+//   name: "Dahnu",
+//   lead: "vijay",
 
-  introduce() {
-    return `${this.lead} performs in ${this.name}`;
-  },
-};
-console.log(bollywood.introduce());
+//   introduce() {
+//     return `${this.lead} performs in ${this.name}`;
+//   },
+// };
+// console.log(bollywood.introduce());
 
-const filmDirector = {
-  name: "Sanjay",
-  cast: ["ranveer", "vijay", "sondaraya"],
+// const filmDirector = {
+//   name: "Sanjay",
+//   cast: ["ranveer", "vijay", "sondaraya"],
 
-  announceCast() {
-    this.cast.forEach((actor) => {
-      console.log(`${this.name} introduce ${actor}`);
-    });
-  },
-};
-filmDirector.announceCast();
+//   announceCast() {
+//     this.cast.forEach((actor) => {
+//       console.log(`${this.name} introduce ${actor}`);
+//     });
+//   },
+// };
+// filmDirector.announceCast();
 
-const filmSet = {
-  crew: "spot boys",
-  prepareProps() {
-    console.log(`outer this crew : ${this.crew}`);
-    function arrangement() {
-      console.log(`INNNER this.crew : ${this.crew}`);
-    }
-    arrangement();
+// const filmSet = {
+//   crew: "spot boys",
+//   prepareProps() {
+//     console.log(`outer this crew : ${this.crew}`);
+//     function arrangement() {
+//       console.log(`INNNER this.crew : ${this.crew}`);
+//     }
+//     arrangement();
 
-    arrangeLight = () => {
-      console.log(`Arraw this.crew : ${this.crew}`);
-    };
-    arrangeLight();
-  },
-};
-filmSet.prepareProps();
+//     arrangeLight = () => {
+//       console.log(`Arraw this.crew : ${this.crew}`);
+//     };
+//     arrangeLight();
+//   },
+// };
+// filmSet.prepareProps();
 
-// detached method
+// // detached method
 
-const actor = {
-  name: "ranveer",
-  bow() {
-    return `${this.name} takes a bow`;
-  },
-};
+// const actor = {
+//   name: "ranveer",
+//   bow() {
+//     return `${this.name} takes a bow`;
+//   },
+// };
 
-const detachedBow = actor.bow;
-console.log(detachedBow());
+// const detachedBow = actor.bow;
+// console.log(detachedBow());
 
 // notes
 // this ka global state kya hota hai
@@ -71,3 +71,14 @@ console.log(detachedBow());
 // 4)normal function me under agr nested regural hn hai tb this ke pass variable ka access nhi hoga.
 // 5)But if there is an arrow fn inside normalfn then, this variable ka reference lega.
 // 6)detached method me bhi this variable ka refernce nhi leta
+
+
+
+const ranveer = {
+  name: "ranveer",
+  cast: "Actor",
+  introduce() {
+    return `My name is ${this.name} and I am an ${this.cast}`;
+  }
+}
+console.log(ranveer.introduce());
